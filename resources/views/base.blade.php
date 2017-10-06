4f30017128475308539146b28e17d17ebe00bcff
Marius Kießling <github@mariuskiessling.de>
2017-10-06 17:49:32 +0200

    Initial commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Base</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/fonts/elegant_font.css">
+    </head>
+    <body>
+        <nav>
+            <div id="logo">
+                <span class="highlighted">D</span>igitales <span class="highlighted">M</span>useum
+            </div>
+
+            <div id="search-bar">
+                <span class="icon icon_search"></span>
+                <input type="text" name="search_query" placeholder="Search the museum">
+            </div>
+
+            <div id="user-information">
+                <span id="welcome-message">Willkommen zurück, Johannes!</span>
+
+                <a href="/logout" id="logout-icon">
+                    <span class="icon icon_lock_alt"></span>
+                </a>
+            </div>
+        </nav>
+
+        <div id="content">
+            <div id="site-title">
+                <div id="site-title-wrapper">
+                    <span id="site-title-label">People</span>
+                </div>
+            </div>
+            <div id="site-content">
+                PLACE YOUR CODE IN HERE!
+            </div>
+        </div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('timeline');
+});
+
+Route::get('/person', function () {
+    return view('details.person');
+});
+
+Route::get('/epoch', function () {
+    return view('details.epoch');
+});
+
+Route::get('/epochs', function () {
+    return view('epochs');
+});
+
+Route::get('/example', function () {
+    return view('base');
+});

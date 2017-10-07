2557740ca7fc56ac42c44a7f0b940ff97008add5
Emre Besogul <emre@Emres-MacBook-Pro.local>
2017-10-07 12:53:27 +0200

    Update login, epochs and portrait webpage

---
+++ resources/views/login.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/login_emre.css">
+        <link rel="stylesheet" href="/fonts/elegant_font.css">
+    </head>
+    <body>
+
+      <div class="wrapper">
+
+    		<div class="top">
+    			<h1 id="title" class="hidden"><span class="highlighted">D</span>igitales <span class="highlighted">M</span>useum</h1>
+        </div>
+
+    		<div class="login-box">
+    			<div class="box-header">
+    				<h2>Log In</h2>
+    			</div>
+    			<label for="username">Username</label>
+    			<br/>
+    			<input type="text" id="username" required>
+    			<br/>
+    			<label for="password">Password</label>
+    			<br/>
+    			<input type="password" id="password" required>
+    			<br/>
+    			<button type="submit">Sign In</button>
+    			<br/>
+    		</div>
+    	</div>
+
+
+    </body>
+</html>

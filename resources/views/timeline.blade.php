bc4126d28f138c9015b539f0cc266ceb451ac344
Maendle <johannes.maendle@hpe.com>
2017-10-07 15:06:24 +0200

    New feature "Timeline" has been created. An additional CSS file for the page has also been created.

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,1 +1,116 @@
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Base</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/johannes_timeline.css" />
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/fonts/elegant_font.css">
+    </head>
+    <body>
+        <nav>
+            <div id="logo">
+                <span class="highlighted">D</span>igitales <span class="highlighted">M</span>useum
+            </div>
+
+            <div id="search-bar">
+                <span class="icon icon_search"></span>
+                <input type="text" name="search_query" placeholder="Search the museum">
+            </div>
+
+            <div id="user-information">
+                <span id="welcome-message">Willkommen zurück, Johannes!</span>
+
+                <a href="/logout" id="logout-icon">
+                    <span class="icon icon_lock_alt"></span>
+                </a>
+            </div>
+        </nav>
+
+        <div id="content">
+            <div id="site-title">
+                <div id="site-title-wrapper">
+                    <span id="site-title-label">People</span>
+                </div>
+            </div>
+            <div id="site-content">
+              <section class="timeline">
+                <ul>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2010</time> Test Test Test Test Test
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2010</time> Test Test Test Test Test In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est. In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                          <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+                  <li>
+                    <div class="timelineEntry">
+                      <div class="timelinePicture">
+                        <img src = "https://pbs.twimg.com/profile_images/655015593276477441/LmuMXwW4.jpg" style='height: 100%; width: 100%; object-fit: contain'/>
+                      </div>
+                      <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                    </div>
+                  </li>
+
+
+              </section>
+
+            </div>
+        </div>
+          <script language="javascript" type="text/javascript" src="/js/johannes_timeline.js"></script>
+    </body>
+</html>

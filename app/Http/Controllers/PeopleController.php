db18e0ccb00219e1d017d8d33de525c04e0d6188
Konstantin Kläger <konstantin_klaeger@web.de>
2017-10-20 13:34:22 +0200

    Send epochs to personCreate and personEdit

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -43,7 +43,10 @@ class PeopleController extends Controller
     {
         if(parent::userIsAuthenticated() && parent::userIsAdmin())
         {
-            return view('admin.personCreate');
+            $epochs = DB::select('SELECT id, name
+                FROM epochs');
+
+            return view('admin.personCreate', ['epochs' => json_decode(json_encode($epochs),true)]);
 
         } else
         {
@@ -391,7 +394,23 @@ class PeopleController extends Controller
                 'id' => $id
                 ]);
 
-            return view('admin.personEdit',  ['id' => $result[0]->id, 'name' => $result[0]->name, 'birthday' => $result[0]->birthday, 'location' => $result[0]->location, 'date_of_death' => $result[0]->date_of_death, 'short_description' => $result[0]->short_description, 'videos' => json_decode(json_encode($videos),true), 'portrait_filename' => $result[0]->portrait_filename, 'poster_filename' => $result[0]->poster_filename, 'texts' => json_decode(json_encode($texts),true), 'pictures' => json_decode(json_encode($pictures),true)]);
+            $epochs = DB::select('SELECT id, name
+                FROM epochs');
+
+            return view('admin.personEdit',  [
+                'id' => $result[0]->id,
+                'name' => $result[0]->name,
+                'birthday' => $result[0]->birthday,
+                'location' => $result[0]->location,
+                'date_of_death' => $result[0]->date_of_death,
+                'short_description' => $result[0]->short_description,
+                'videos' => json_decode(json_encode($videos),true),
+                'portrait_filename' => $result[0]->portrait_filename,
+                'poster_filename' => $result[0]->poster_filename,
+                'texts' => json_decode(json_encode($texts),true),
+                'pictures' => json_decode(json_encode($pictures),true),
+                'epochs' => json_decode(json_encode($epochs),true)
+                ]);
 
             } else
             {
